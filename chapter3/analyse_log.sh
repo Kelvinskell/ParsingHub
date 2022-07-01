e378603c58178da3f39bcb79c14b98a4359b2471
Kelvinskell <kelvinskelll@gmail.com>
2022-07-01 15:40:28 +0100

    Add file validation check

---
+++ chapter3/analyse_log.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Accept and validate filename argument
+if [[ $* < 1 ]]
+then
+	echo "No Logfile supplied. Using default file"
+	if [[ ! -f apache_logs.txt ]]
+	then
+		echo -e "Default logfile (apache_logs.txt) no longer exists. \nExiting..."
+		exit $?
+	else
+		logfile=apache_logs.txt
+	fi
+else
+	if [[ ! -f $1 ]]
+	then
+		echo -e "$1: No such file \nExiting..."
+		exit $?
+	else
+		logfile=$1
+	fi
+fi
