79f4d4b9d7b790d3a5ea6c3780e1a1931e2610a3
Kelvinskell <kelvinskelll@gmail.com>
2022-07-02 15:13:43 +0100

    Use pyshark to analyse dns.cap

---
+++ chapter4/cap2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pyshark
+
+cap = pyshark.FileCapture("dns.cap")
+
+# Activate packet capture
+try:
+    cap[10000]
+except KeyError:
+    pass
+
+# Find IP Address of www.netbsd.org
+for packet in range(len(cap)):
+    if cap[packet].dns.qry_name == 'www.netbsd.org':
+        address = cap[packet].ip.addr
+        print("The IP Address of www.netbsd.org is:", address)
+        break
+
+# For what domain was there a mail excahnge lookup?
+for packet in range(len(cap)):
+    if 'MX' in cap[packet].dns.qry_type.showname_value:
+        print("{} has a mail excahnge lookup".format(cap[packet].dns.qry_name))
+        break
+
