57bc7d8c2e69c28642e50311ca4c04ec332d80d6
Kelvinskell <kelvinskelll@gmail.com>
2022-07-02 15:32:57 +0100

    Analyse dns.cap with pyshark

--- chapter4/cap2.py
+++ chapter4/cap2.py
@@ -23,3 +23,17 @@ for packet in range(len(cap)):
         print("{} has a mail excahnge lookup".format(cap[packet].dns.qry_name))
         break
 
+# Name a mail exchange server for google.com
+for packet in range(len(cap)):
+    if cap[packet].dns.qry_name == 'google.com' and 'mx_mail_exchange' in dir(cap[packet].dns):
+        server = cap[packet].dns.mx_mail_exchange
+        print("www.google.com has the mail exchange server:", server)
+        break
+
+# For what domain was there a nameserver lookup?
+for packet in range(len(cap)):
+    if 'NS' in cap[packet].dns.qry_type.showname_value:
+        print("{} has a Name Server lookup".format(cap[packet].dns.qry_name))
+        break
+
+
