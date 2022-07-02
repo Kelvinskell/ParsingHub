e6dfbe8c8df005f1e2751b301fd8f4f9b67412a1
Kelvinskell <kelvinskelll@gmail.com>
2022-07-02 12:25:28 +0100

    Improve valiidate function

--- chapter4/cap.py
+++ chapter4/cap.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import pyshark
 from pathlib import Path
@@ -7,10 +8,19 @@ from pathlib import Path
 # Print the average time in miliseconds between two packets
 
 # Read and validate filename argument if provided
-if len(sys.argv) > 1:
-    filename = sys.argv[1] 
-    #Check if file is a capture file
-    if not Path(filename).suffix == ".cap":
-        print("{} is not a capture file.".format(filename))
-        sys.exit
+def validate():
+    if len(sys.argv) > 1:
+        filename = sys.argv[1] 
+
+        # Check if file exists
+        if not os.path.isfile(filename):
+            print("{}: File does not exist".format(filename))
+            sys.exit()
+
+        # Check if file is a capture file
+        if not Path(filename).suffix == ".cap":
+            print("{} is not a capture file.".format(filename))
+            sys.exit
+
+validate()
 
